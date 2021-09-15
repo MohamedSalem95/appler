3717b103ecd9b0147e6636efd4fb61528ba2a62d
mohamed <drakelogan52@gmail.com>
2021-09-15 15:34:02 +0200

    add notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_130818) do
+ActiveRecord::Schema.define(version: 2021_09_15_133157) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_09_15_130818) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.string "notes"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
